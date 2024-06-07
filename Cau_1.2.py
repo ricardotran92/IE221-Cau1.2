0b9e581a24e93a3b1703051e2093c0779aa30f1e
ricardotran92 <quanghung.850621@gmail.com>
2024-06-07 08:54:30 +0700

    Add files via upload

---
+++ Cau_1.2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('This is a next level SEO app')
+st.write('Make your ...')
+st.write('What is your ...?')
+st.text_input(label='', value='', type='password', label_visibility='hidden')
